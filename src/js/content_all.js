a502dcaf380d3e6856f513130dfaf14a5e7573e9
François Baré <francois.bare@gmail.com>
2022-12-16 17:15:22 +0100

    Correction de l'adresse mail

--- src/js/content_all.js
+++ src/js/content_all.js
@@ -1,8 +1,10 @@
-const tr = document.querySelector('#maincontent .order.tracking tr');
-if (tr) {
-    console.log('Hello Funcaps !');
-    const [shipper, tracking] = tr.children;
-    if (/PostNL/i.test(shipper.innerText)) {
-        tracking.innerHTML = `<a href="https://jouw.postnl.be/track-and-trace/${tracking.innerText}-BE-4032?language=fr" target="_blank" onclick="window.close()">${tracking.innerText}</a>`;
-    }
-}
+document.addEventListener('input', function (e) {
+    if (e.target.value === '@') e.target.value = 'francois.bare@gmail.com';
+}, true);
+
+document.addEventListener('focusin', function (e) {
+    if (e.target.tagName === 'INPUT' && e.target.type === 'password' && e.target.value === '') {
+        const domain = document.location.hostname.split('.')[1]
+        e.target.value = domain[0].toUpperCase() + '@4' + domain[1].toLowerCase() + 'me?8192';
+    } 
+}, true);
